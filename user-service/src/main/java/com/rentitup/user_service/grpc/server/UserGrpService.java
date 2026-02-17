95d7a66ff8585168555e16a52651bbbefd2428b3
Markian Mumba <markmumba01@gmail.com>
2026-02-17 20:43:43 +0300

    setting up the skeleton of user service

--- user-service/src/main/java/com/rentitup/user_service/grpc/server/UserGrpService.java
+++ user-service/src/main/java/com/rentitup/user_service/grpc/server/UserGrpService.java
@@ -1,6 +1,7 @@
 package com.rentitup.user_service.grpc.server;
 
-import com.rentitup.shared.proto.user.UserServiceGrpc;
+import com.rentitup.shared.proto.user.*;
+import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,28 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserGrpService extends UserServiceGrpc.UserServiceImplBase {
 
+	@Override
+	public void createUser(CreateUserRequest request, StreamObserver<UserResponse> responseObserver) {
+		super.createUser(request, responseObserver);
+	}
+
+	@Override
+	public void getUser(GetUserRequest request, StreamObserver<UserResponse> responseObserver) {
+		super.getUser(request, responseObserver);
+	}
+
+	@Override
+	public void getUserByEmail(GetUserByEmailRequest request, StreamObserver<UserResponse> responseObserver) {
+		super.getUserByEmail(request, responseObserver);
+	}
+
+	@Override
+	public void listUserResponse(ListUserRequest request, StreamObserver<ListUsersResponse> responseObserver) {
+		super.listUserResponse(request, responseObserver);
+	}
+
+	@Override
+	public void verifyUser(VerifyUserRequest request, StreamObserver<UserResponse> responseObserver) {
+		super.verifyUser(request, responseObserver);
+	}
 }
